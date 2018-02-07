37ab561495e34392f0837eb78db3973bb50c9eac
e-henry <e-henry@users.noreply.github.com>
2018-02-07 11:05:25 +0100

    Ajout etudiant rar

--- fiches-Appel.sh
+++ fiches-Appel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for eleve in ana tcso dga oou aam mbo ach oel asm nse ael ada EUL abe nbe hhr
+for eleve in ana tcso dga oou aam mbo ach oel asm nse ael ada EUL abe nbe hhr rar
 do
    cp edl.md $eleve.md
    sed -i -e "s/edl/$eleve/g" $eleve.md
